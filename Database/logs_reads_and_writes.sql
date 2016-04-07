c55234df588844b750640dc551e455cb3242e2fb
rsatger <remy.satger@axa-im.com>
2016-04-07 15:03:17 +0800

    add db

---
+++ Database/logs_reads_and_writes.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[logs_reads_and_writes] (
+    [line_id]         INT           IDENTITY (1, 1) NOT NULL,
+    [UTC_timestamp]   DATETIME      NULL,
+    [log_id]          INT           NULL,
+    [user_name]       VARCHAR (15)  NULL,
+    [content]         VARCHAR (100) NULL,
+    [message_type_id] INT           NULL,
+    PRIMARY KEY CLUSTERED ([line_id] ASC),
+    FOREIGN KEY ([message_type_id]) REFERENCES [dbo].[message_type] ([type_id])
+);
+
---
+++ Database/message_type.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[message_type] (
+    [type_id] INT          NOT NULL,
+    [type]    VARCHAR (15) NULL,
+    PRIMARY KEY CLUSTERED ([type_id] ASC)
+);
